f4d81f3104b02725ab8b4612cbfcb930eb206891
Devin Vering <89165809+Devster41@users.noreply.github.com>
2022-10-31 14:10:38 -0400

    Added api call

--- src/components/account-profile-details.js
+++ src/components/account-profile-details.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Box,
   Button,
@@ -10,8 +10,17 @@ import {
   TextField
 } from '@mui/material';
 
+import apiFunctions from '../firebase/api';
+import { ref, onValue } from "firebase/database";
+
+
+
+
 
 export const AccountProfileDetails = (props) => {
+  const [isLoading, setLoading] = useState(true);
+
+  
   const [values, setValues] = useState({
     name: 'Example name',
     email: 'Example email',
@@ -25,6 +34,38 @@ export const AccountProfileDetails = (props) => {
       [event.target.name]: event.target.value
     });
   };
+
+  const fetchData = (event) => {
+    console.log("hello")
+    // Update the document title using the browser API
+    // const response = onValue(await ref(apiFunctions.db, 'tasks/'), (response))
+    // console.log("response: " + response)
+    try {
+        onValue(ref(apiFunctions.db, 'users/'), (snapshot) => {
+                const taskTemp = []
+    
+                snapshot.forEach(function(child) {
+                    const retrievedUser = snapshot.val();
+                    values.push(retrievedUser.name)
+                    taskTemp.push(child.val())
+                })
+  
+        })
+        
+  
+    }
+    catch {
+        // if there is no internet
+    }
+    
+  
+    return true;
+  };
+
+  useEffect(() => {
+    console.log("reload")
+    fetchData()
+}, []);
 
   return (
     <form
