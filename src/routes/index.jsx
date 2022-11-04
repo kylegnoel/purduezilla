e4786caf2fe505ea820aa9e8acd54923d978a7c5
Derek Lee <leederek007@gmail.com>
2022-11-04 08:41:12 -0400

    fix compile error (#35)

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -34,8 +34,8 @@ const Routing = props => {
                 <Route exact path="/mygroups" element={<PrivateRoute redirectRoute="/" ><Groups /></PrivateRoute>} />
                 <Route exact path="*" element={<NotFound />} />
                 <Route exact path="/project/:id" forceRefresh={true} element={<Projects />} />
-                <Route exact path="/task/:id" element={<PrivateRoute condition={isLoggedIn != null} redirectRoute="/" ><Task /></PrivateRoute>} />
-                <Route exact path="/newtask" element={<PrivateRoute condition={isLoggedIn != null} redirectRoute="/" ><AddTaskPage /></PrivateRoute>} />
+                <Route exact path="/task/:id" element={<PrivateRoute redirectRoute="/" ><Task /></PrivateRoute>} />
+                <Route exact path="/newtask" element={<PrivateRoute redirectRoute="/" ><AddTaskPage /></PrivateRoute>} />
                 <Route exact path="/newtask/:id" element={<AddTaskPage />} />
                 <Route exact path="/mytasks" element={<Task />} />
             </Routes>
